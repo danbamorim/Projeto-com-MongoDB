d2181eefa1ba55906f5e5152dbaf1c5db0bdba43
Danilo Amorim <daniloamorim@MacBook-Pro-de-Danilo.local>
2024-01-19 09:23:23 -0300

    atualizando os erros

---
+++ db/conn.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require("mongodb");
+// Connection URI
+const uri = "mongodb://localhost:27017/testemongodb";
+// Create a new MongoClient
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    console.log("Conectado ao MongoDB!");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+run();
+
+
+
+
+//conexao com banco mongodb
+module.exports = client;
